fcd10d2fb739fa2a3e289153e61299d4f809dd08
Giulia Pasquale <giulia.pasquale@iit.it>
2015-08-30 19:23:16 -0400

    added code to synch images and joint vectors

---
+++ hand_multisensory/regression_caffe/hdf5creation/synchronize.m
@@ -0,0 +1,131 @@
+%%
+
+path_vector = '/media/giulia/DATA/humanoids2015/dumpings/bbball2/pf3dTracker_data/data.log';
+
+fid = fopen(path_vector,'r');
+if (fid==-1)
+    error('Error!');
+end
+
+if isunix
+    [~,vector_count] = system(['wc -l < ' path_vector]);
+    vector_count = str2num(vector_count);
+elseif ispc
+    [~,vector_count] = system(['find /v /c "&*fake&*" "' path_vector '"']);
+    last_space = strfind(vector_count,' ');
+    vector_count = str2num(vector_count((last_space(end)+1):(end-1)));
+end
+
+vector_cell = textscan(fid, '%d %f %f %f %f %f %d %d %d', vector_count);
+vector_cell(1) = [];
+
+fclose(fid);
+
+%%
+
+path_image = '/media/giulia/DATA/humanoids2015/dumpings/bbball2/nearBlobber_blobs/data.log';
+
+fid = fopen(path_image,'r');
+if (fid==-1)
+    error('Error!');
+end
+
+if isunix
+    [~,img_count] = system(['wc -l < ' path_image]);
+    img_count = str2num(img_count);
+elseif ispc
+    [~,img_count] = system(['find /v /c "&*fake&*" "' path_image '"']);
+    last_space = strfind(img_count,' ');
+    img_count = str2num(img_count((last_space(end)+1):(end-1)));
+end
+
+image_cell = textscan(fid, '%d %f %d %d %d', img_count);
+image_cell(1) = [];
+
+fclose(fid);
+
+Tstart = 1435307624.000000;
+
+image_cell{1} = image_cell{1} - Tstart;
+vector_cell{1} = vector_cell{1} - Tstart - 1.21;
+
+%%
+
+time_window = 5;
+
+path_association = '/media/giulia/DATA/humanoids2015/dumpings/bbball2/pfdata_blobberblobs/data_blobs.txt';
+path_vector = '/media/giulia/DATA/humanoids2015/dumpings/bbball2/pfdata_blobberblobs/data.txt';
+path_image = '/media/giulia/DATA/humanoids2015/dumpings/bbball2/pfdata_blobberblobs/blobs.txt';
+
+fid = fopen(path_association,'w');
+if (fid==-1)
+    error('Error!');
+end
+
+fid_pf = fopen(path_vector,'w');
+if (fid_pf==-1)
+    error('Error!');
+end
+
+fid_disp = fopen(path_image,'w');
+if (fid_disp==-1)
+    error('Error!');
+end
+
+ass_cell = zeros(2, 2, img_count);
+ass_timestamp = zeros(img_count, 2);
+
+for idx_img = 1:img_count
+    
+    t_image = image_cell{1}(idx_img);
+    
+    if idx_img==1
+        t_diff = vector_cell{1} - t_image;
+        [min_diff, start_idx_vector] = min(abs(t_diff));
+    else
+        end_idx_vector = min(start_idx_vector + (time_window-1), length(vector_cell{1}));
+        t_diff = vector_cell{1}(start_idx_vector:end_idx_vector) - t_image;
+        flag = 1;
+        if t_diff(1)<0 && t_diff(end)>=0
+            [min_diff, idx_vector] = min(abs(t_diff));
+            start_idx_vector = (start_idx_vector-1) + idx_vector;
+            flag = 0;
+        end
+        if t_diff(1)>=0
+            flag = 0;
+        end
+        while (t_diff(end)<0 && end_idx_vector<length(vector_cell{1}))
+            start_idx_vector = end_idx_vector;
+            end_idx_vector = min(start_idx_vector + time_window, length(vector_cell{1}));
+            t_diff = vector_cell{1}(start_idx_vector:end_idx_vector) - t_image;
+            if t_diff(1)<0 && t_diff(end)>=0
+                [min_diff, idx_vector] = min(abs(t_diff));
+                start_idx_vector = start_idx_vector + idx_vector;
+                flag = 0;
+                break;
+            end
+            if t_diff(1)>=0
+                flag = 0;
+                break;
+            end
+        end
+        if flag
+            start_idx_vector = end_idx_vector;
+        end
+    end
+    
+    ass_timestamp(idx_img, 1) = vector_cell{1}(start_idx_vector);
+    ass_timestamp(idx_img, 2) = image_cell{1}(idx_img);
+    
+    ass_cell(1, :, idx_img) = [image_cell{2}(idx_img) image_cell{3}(idx_img)];
+    ass_cell(2, :, idx_img) = [vector_cell{6}(start_idx_vector) vector_cell{7}(start_idx_vector)];
+    
+    fprintf(fid, '%.6f %d %d %.6f %d %d\n', ass_timestamp(idx_img, 1), ass_cell(1, 1, idx_img), ass_cell(1, 2, idx_img), ass_timestamp(idx_img, 2), ass_cell(2, 1, idx_img), ass_cell(2, 2, idx_img));
+    fprintf(fid_disp, '%.6f %d %d\n', ass_timestamp(idx_img, 1), ass_cell(1, 1, idx_img), ass_cell(1, 2, idx_img));
+    fprintf(fid_pf, '%.6f %d %d\n', ass_timestamp(idx_img, 2), ass_cell(2, 1, idx_img), ass_cell(2, 2, idx_img));
+    
+end
+
+fclose(fid);
+fclose(fid_disp);
+fclose(fid_pf);
